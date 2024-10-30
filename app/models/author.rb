8aabc2051b2849f2b768744515de4bde961c08f2
WillBurgessBowyer <wburgess-bowyer@academic.rrc.ca>
2024-10-29 22:17:54 -0500

    Updated seed.rb to populate db with 200 rows

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,6 @@
-class Category < ApplicationRecord
+class Author < ApplicationRecord
   has_many :books
   
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :biography, length: { minimum: 10 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,37 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Clear data
+Book.destroy_all
+Author.destroy_all
+Category.destroy_all
+
+# Create Categories
+10.times do
+  Category.create(name: Faker::Book.genre)
+end
+puts "Created #{Category.count} categories."
+
+# Create Authors
+40.times do
+  Author.create(
+    name: Faker::Book.author,
+    biography: Faker::Lorem.paragraph(sentence_count: 5),
+    birthdate: Faker::Date.birthday(min_age: 25, max_age: 80)
+  )
+end
+puts "Created #{Author.count} authors."
+
+# Create Books
+target_books = 150
+current_books = Book.count
+
+while current_books < target_books
+  book = Book.create(
+    title: Faker::Book.title,
+    description: Faker::Lorem.paragraph(sentence_count: 3),
+    published_date: Faker::Date.backward(days: 5000),
+    author: Author.all.sample,
+    category: Category.all.sample
+  )
+  current_books = Book.count if book.persisted?
+end
+puts "Created #{Book.count} books."
+
