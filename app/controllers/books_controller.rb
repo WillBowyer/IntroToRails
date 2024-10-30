0476a1dea7d809d04d0be10e6b54fed7a44fc131
WillBurgessBowyer <wburgess-bowyer@academic.rrc.ca>
2024-10-30 18:37:08 -0500

    Fixed the search to be compatible with SQLite

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,14 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.page(params[:page]).per(10)
+    if params[:query].present?
+      query = "%#{params[:query].downcase}%"
+      @books = Book.joins(:author, :category)
+                   .where("LOWER(books.title) LIKE ? OR LOWER(authors.name) LIKE ? OR LOWER(categories.name) LIKE ?", 
+                          query, query, query)
+                   .page(params[:page]).per(10)
+    else
+      @books = Book.page(params[:page]).per(10)
+    end
   end
 
   def show
