168202ed6e7af71bcf8a10d5e2ccd3db937eb507
WillBurgessBowyer <wburgess-bowyer@academic.rrc.ca>
2024-10-29 22:00:44 -0500

    Added Validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
-class Author < ApplicationRecord
+class Category < ApplicationRecord
+  has_many :books
+  
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :author
   belongs_to :category
+
+  validates :title, presence: true, uniqueness: true
+  validates :published_date, presence: true
 end
