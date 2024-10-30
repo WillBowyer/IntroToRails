da9a5de4545a4fd07a1917fb3d6f8ad5610b47cd
WillBurgessBowyer <wburgess-bowyer@academic.rrc.ca>
2024-10-29 22:59:35 -0500

    Added show page for individual book entries

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "books/index"
   root 'static_pages#home'
   get 'about', to: 'static_pages#about'
-  resources :books, only: [:index]
+  resources :books, only: [:index, :show]
+
 
 end
