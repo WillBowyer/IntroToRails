f3149b27430a0feb37225ebc9a569882076633d3
WillBurgessBowyer <wburgess-bowyer@academic.rrc.ca>
2024-11-01 10:03:08 -0500

    Generated Catergories Controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,19 @@
 # Last Updated: 1-11-24
 
 Rails.application.routes.draw do
-  get "books/index"
+  get "categories/index"
+  get "categories/show"
+  
   root 'static_pages#home'
   get 'about', to: 'static_pages#about'
+
   resources :books, only: [:index, :show]
 
+  resources :categories, only: [:index, :show] do
+    resources :books, only: [:index, :show]
+  end
 
+  resources :authors, only: [:index, :show] do
+    resources :books, only: [:index, :show]
+  end
 end
